8e87b46e8342e6d36a2cefe3337087b416fa94fb
pooyayeganehfar <79022782+pooyayeganehfar@users.noreply.github.com>
2025-06-03 17:05:13 +0330

    add services to site

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+  return <h1 className="text-2xl font-bold text-center mt-10">درباره ما</h1>;
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,3 @@
+export default function ContactPage() {
+  return <h1 className="text-2xl font-bold text-center mt-10">تماس با ما</h1>;
+}
